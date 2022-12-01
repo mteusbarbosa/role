6367d7a9a6fd6cf9d342ddf6bae9ef312db918ca
mteusbarbosa <mateus.bm13@gmail.com>
2022-12-01 18:00:06 -0300

    Método Delete no banco

--- crud-spring/src/main/java/com/mateus/controller/RoleController.java
+++ crud-spring/src/main/java/com/mateus/controller/RoleController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -64,6 +65,17 @@ public class RoleController {
                     recordFound.setRoleUrl(role.getRoleUrl());
                     Role updated = roleRepository.save(recordFound);
                     return ResponseEntity.ok().body(updated);
+                })
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    //Método delete
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        return roleRepository.findById(id)
+                .map(recordFound -> {
+                    roleRepository.deleteById(id);
+                    return ResponseEntity.noContent().<Void>build();
                 })
                 .orElse(ResponseEntity.notFound().build());
     }
